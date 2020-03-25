f6eeaa594645f8b012cdd9efb6a2cb7f41003c8f
zhiyuan <86562713@qq.com>
2020-03-25 10:53:34 +0800

    init DataSet.

--- apdt/general.py
+++ apdt/general.py
@@ -31,3 +31,74 @@ class SubThread(threading.Thread):
             return self.result
         except Exception:
             return None
+
+class DataSet():
+    '''DataSet can make DataPack object being Machine Learning ready.
+    '''
+    def __init__(self, datapack, **kwarg):
+        if 'time-aligned' not in datapack.tag:
+            raise Exception("datapack should be aligned along time axis first.")
+        
+        if hasattr(kwarg['method'], '__call__'):
+            kwarg['method'](datapack, **kwarg)
+        elif kwarg['method'] in ['window']:
+            if kwarg['method'] == 'window':
+                self._construct_window(datapack, **kwarg)
+        else:
+            raise Exception("method should be predefined or a callable.")
+
+    def _construct_window(self, datapack, **kwarg):
+        T = datapack.time_length
+        N = datapack.site_num
+        self.data = datapack.data.reset_index().sort_values(['datetime','site_id'])['data0'].values.reshape((T,N,1))
+
+        batch_perm = np.linspace(0, N-1, N)
+        np.random.shuffle(batch_perm)
+        self.data = self.data[:,batch_perm.astype(int),:]
+
+        self.tr = self.data[:81*108+1]
+        self.te = self.data[81*108+1:81*216+2]
+        self.tr = np.transpose(self.tr,(1,0,2))
+        self.te = np.transpose(self.te,(1,0,2))
+        self.tr = np.concatenate((self.tr,np.concatenate((self.tr,self.tr[:,-1:]),1)[:,1:,1:]),-1)
+        self.te = np.concatenate((self.te,np.concatenate((self.te,self.te[:,-1:]),1)[:,1:,1:]),-1)
+
+        self.batch_size = args['seq_len']-1
+        self.tr_batch_counter = 0
+        self.tr_batch_num = (self.tr.shape[1]-1)//(args['seq_len']-1)
+        self.tr_batch_perm = np.linspace(0,self.tr_batch_num-1,self.tr_batch_num)
+        np.random.shuffle(self.tr_batch_perm)
+        self.te_batch_counter = 0
+        self.te_batch_num = (self.te.shape[1]-1)//(args['seq_len']-1)
+        self.te_batch_perm = np.linspace(0,self.te_batch_num-1,self.te_batch_num)
+        np.random.shuffle(self.te_batch_perm)
+
+    def tr_get_batch(self,id=None):
+        if id:
+            idx_start = self.batch_size*id
+            idx_end = self.batch_size*(id+1)
+        else:
+            id = int(self.tr_batch_perm[self.tr_batch_counter])
+            idx_start = self.batch_size*id
+            idx_end = self.batch_size*(id+1)
+            self.tr_batch_counter = (self.tr_batch_counter+1)%self.tr_batch_num
+            if self.tr_batch_counter==0:
+                np.random.shuffle(self.tr_batch_perm)
+        
+        batch = self.tr[:,idx_start:idx_end+1]
+        return batch
+
+    def te_get_batch(self,id=None):
+        if id:
+            idx_start = self.batch_size*id
+            idx_end = self.batch_size*(id+1)
+        else:
+            id = int(self.te_batch_perm[self.te_batch_counter])
+            idx_start = self.batch_size*id
+            idx_end = self.batch_size*(id+1)
+            self.te_batch_counter = (self.te_batch_counter+1)%self.te_batch_num
+            if self.te_batch_counter==0:
+                np.random.shuffle(self.te_batch_perm)
+        
+        batch = self.te[:,idx_start:idx_end+1]
+        return batch
--- script.py
+++ script.py
@@ -4,5 +4,5 @@ import numpy as np
 t = apdt.io.load_nms('ALL','2017-01-01','2017-01-02')
 r = apdt.alz.find_sites_range(t, 39.8673, 116.3660, 100)
 t = apdt.proc.spatial_interplot(t, 'IDW')
-t = apdt.proc.temporal_interpolate(t, 'ALL')
+t = apdt.proc.temporal_interpolate(t, 'ALL', 'linear')
 _debug = 233
