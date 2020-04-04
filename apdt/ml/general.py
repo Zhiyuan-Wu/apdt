da7483491b085586665977695f58f2416bb46334
zhiyuan <86562713@qq.com>
2020-04-04 22:53:20 +0800

    Remake ml.DataSet class.

--- apdt/ml/general.py
+++ apdt/ml/general.py
@@ -7,79 +7,107 @@ import apdt
 
 class DataSet():
     '''DataSet can make DataPack object being Machine Learning ready.
+    Description
+    -----------
+        Dataset object consist of three parts: pre_process, get_data, and post_process.
+
+        - pre_process is to make DataPack obejct into a ML-ready ndarray self.tr and self.te, which have size N*D_1*D_2*...; The first dimension is considered as independent sample index; Beside this, two variable self.tr_batch_num and self.te_batch_num which claims the coressponding sample number should also be defined.
+        - get_data has two key methods tr_get_batch and te_get_batch, will return a B*D_1*D_2*... as a batch of training/testing data.
+        - post_process defined the action that after every get_data call.
+
+        In most case users have to re-define pre_process function to make their desired dataset structure. We also provide some pre-defined useful pre_process function for you. get_data and post_process typically don't need much work, but they can still be re-defined if you like.
+
+    Parameters
+    ----------
+        datapack: DataPack object
+        method: str
+            If this argument is defined, we will use sepecific pre-defined pre_process fuction.
+            Avaliable:
+            - 'window': use a non-overlapping window across time-axis to generate samples. locations in a time slice are supposed to be same (i.e. fixed-station). a samlple will be a N*T*D array. where N is the number of locations, T is window length and D is feature dimensions.
+
     '''
     def __init__(self, datapack, **kwarg):
-        if 'time-aligned' not in datapack.tag:
-            raise Exception("datapack should be aligned along time axis first.")
-        
-        if hasattr(kwarg['method'], '__call__'):
-            kwarg['method'](datapack, **kwarg)
-        elif kwarg['method'] in ['window']:
-            if kwarg['method'] == 'window':
-                self._construct_window(datapack, **kwarg)
-        else:
-            raise Exception("method should be predefined or a callable.")
+        self.pre_process(datapack, **kwarg)
+        self._init_counter(**kwarg)
 
-    def _construct_window(self, datapack, **kwarg):
-        self.method = 'window'
-        
-        if 'split_ratio' not in kwarg.keys():
-            kwarg['split_ratio'] = 0.7
-        if 'shuffle' not in kwarg.keys():
-            kwarg['shuffle'] = True
-        if 'seq_len' not in kwarg.keys():
-            kwarg['seq_len'] = 100
-        if 'normalize' not in kwarg.keys():
-            kwarg['normalize'] = True
+    def pre_process(self, datapack, **kwarg):
+        if 'method' not in kwarg.keys():
+            raise Exception("You have to re-define pre_process function or use a pre-defined one using 'method' argument.")
 
-        T = datapack.time_length
-        N = datapack.site_num
-        if kwarg['normalize']:
-            datapack = apdt.proc.linear_normalize(datapack, 'data0', '99pt')
-        self.data = datapack.data.reset_index().sort_values(['datetime', 'site_id'])['data0'].values.reshape((T, N, 1))
+        if kwarg['method'] == 'window':
+            if 'time-aligned' not in datapack.tag:
+                raise Exception("datapack should be aligned along time axis first.")
+            if 'fixed-location' not in datapack.tag:
+                raise Exception("window method only support fixed station data.")
+            
+            if 'split_ratio' not in kwarg.keys():
+                kwarg['split_ratio'] = 0.7
+            if 'shuffle' not in kwarg.keys():
+                kwarg['shuffle'] = True
+            if 'seq_len' not in kwarg.keys():
+                kwarg['seq_len'] = 100
+            if 'normalize' not in kwarg.keys():
+                kwarg['normalize'] = True
 
-        if kwarg['shuffle'] is True:
-            batch_perm = np.linspace(0, N-1, N)
-            np.random.shuffle(batch_perm)
-            self.data = self.data[:, batch_perm.astype(int), :]
+            self.method = 'window'
+            T = datapack.time_length
+            N = datapack.site_num
+            if kwarg['normalize']:
+                datapack = apdt.proc.linear_normalize(datapack, 'data0', '99pt')
+            self.data = datapack.data.reset_index().sort_values(['datetime', 'site_id'])['data0'].values.reshape((T, N, 1))
 
-        split_point = int(T * kwarg['split_ratio'])
-        self.tr = self.data[:split_point]
-        self.te = self.data[split_point:]
-        self.tr = np.transpose(self.tr, (1,0,2))
-        self.te = np.transpose(self.te, (1,0,2))
+            split_point = int(T * kwarg['split_ratio'])
+            self.tr = self.data[:split_point]
+            self.te = self.data[split_point:]
+            self.seq_len = kwarg['seq_len']
+            self.tr_batch_num = self.tr.shape[0]//kwarg['seq_len']
+            self.te_batch_num = self.te.shape[0]//kwarg['seq_len']
+            if self.tr_batch_num == 0 or self.te_batch_num == 0:
+                raise Exception("time_length is not enough to construct a window.")
+            
+            self.tr = self.tr[:self.tr_batch_num * self.seq_len]
+            self.te = self.te[:self.te_batch_num * self.seq_len]
+            self.tr = self.tr.reshape((-1, self.seq_len, N, 1))
+            self.te = self.te.reshape((-1, self.seq_len, N, 1))
+            self.tr = np.transpose(self.tr, (0,2,1,3))
+            self.te = np.transpose(self.te, (0,2,1,3))
 
-        self.seq_len = kwarg['seq_len']
+    def _init_counter(self, **kwarg):
+        if 'seed' in kwarg.keys():
+            np.random.seed(kwarg['seed'])
         self.tr_batch_counter = 0
-        self.tr_batch_num = self.tr.shape[1]//kwarg['seq_len']
+        self.te_batch_counter = 0
         self.tr_batch_perm = np.linspace(0,self.tr_batch_num-1,self.tr_batch_num)
         np.random.shuffle(self.tr_batch_perm)
-        self.te_batch_counter = 0
-        self.te_batch_num = self.te.shape[1]//kwarg['seq_len']
         self.te_batch_perm = np.linspace(0,self.te_batch_num-1,self.te_batch_num)
         np.random.shuffle(self.te_batch_perm)
 
-    def tr_get_batch(self):
-        if self.method=='window':
-            id = int(self.tr_batch_perm[self.tr_batch_counter])
-            idx_start = self.seq_len * id
-            idx_end = self.seq_len * (id + 1)
-            self.tr_batch_counter = (self.tr_batch_counter + 1) % self.tr_batch_num
-            if self.tr_batch_counter==0:
-                np.random.shuffle(self.tr_batch_perm)
-            batch = self.tr[:, idx_start:idx_end]
-            return batch
+    def tr_get_batch(self, batch_size=1):
+        if batch_size > self.tr_batch_num:
+            batch_size = self.tr_batch_num
+        if self.tr_batch_counter + batch_size > self.tr_batch_num:
+            np.random.shuffle(self.tr_batch_perm)
+            self.tr_batch_counter = 0
+        target_index = self.tr_batch_perm[self.tr_batch_counter: self.tr_batch_counter + batch_size]
+        self.tr_batch_counter = self.tr_batch_counter + batch_size
+        batch = self.tr[target_index]
+        batch = self.post_process(batch)
+        return batch
+    
+    def te_get_batch(self, batch_size=1):
+        if batch_size > self.te_batch_num:
+            batch_size = self.te_batch_num
+        if self.te_batch_counter + batch_size > self.te_batch_num:
+            np.random.shuffle(self.te_batch_perm)
+            self.te_batch_counter = 0
+        target_index = self.te_batch_perm[self.te_batch_counter: self.te_batch_counter + batch_size]
+        self.te_batch_counter = self.te_batch_counter + batch_size
+        batch = self.te[target_index]
+        batch = self.post_process(batch)
+        return batch
 
-    def te_get_batch(self,id=None):
-        if self.method=='window':
-            id = int(self.te_batch_perm[self.te_batch_counter])
-            idx_start = self.seq_len * id
-            idx_end = self.seq_len * (id + 1)
-            self.te_batch_counter = (self.te_batch_counter + 1) % self.te_batch_num
-            if self.te_batch_counter==0:
-                np.random.shuffle(self.te_batch_perm)
-            batch = self.te[:,idx_start:idx_end]
-            return batch
+    def post_process(self, batch):
+        return batch
 
 class TFModel():
     def __init__(self, **kwarg):
