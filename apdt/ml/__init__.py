596f779ec7e998966d32cf3b8f23914eec4747c5
zhiyuan <zhiyuan@bcm.cm.cluster>
2020-10-17 22:08:24 +0800

    add ml.TFModel.LSTM

--- apdt/ml/__init__.py
+++ apdt/ml/__init__.py
@@ -7,5 +7,5 @@ Provide useful modeling and training interface for deep learning.
 Check https://github.com/Zhiyuan-Wu/apdt for more information.
 '''
 from apdt.ml.general import DataSet, TFModel
-from apdt.ml.tf_model import wavenet_weight, WaveNet, mlp_weight, MLP
+from apdt.ml.tf_model import wavenet_weight, WaveNet, mlp_weight, MLP, lstm_weight, LSTM
 from apdt.ml.tf_model import _WaveNet
